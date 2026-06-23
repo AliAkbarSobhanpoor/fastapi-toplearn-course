c3f86083814c2a8807509ab4a08041f37c9b0df5
aliakbarsobhanpoor <aliakbarsobhanpoor@gmail.com>
2026-06-23 21:55:51 +0330

    create shop project and also updaate .gitignore

--- shop_project/main.py
+++ shop_project/main.py
@@ -1,16 +1,32 @@
-import random
-from string import hexdigits
+from urllib import request
+
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi_standalone_docs import StandaloneDocs
-from starlette.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")  # localhost:8000/static/main.css
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 StandaloneDocs(app)
 
+@app.get("/")
+async def shop(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="product-list.html",
+        context={}
+    )
+
+@app.get("/product/{product_id}")
+async def shop(request: Request, product_id: int):
+    return templates.TemplateResponse(
+        request=request,
+        name="product-detail.html",
+        context={}
+    )
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
